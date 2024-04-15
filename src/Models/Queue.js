f6d608361d5c43241baf758e762937db116cabcf
wilabarca <wilberabarcasanchez078@gmail.com>
2024-04-15 08:37:08 -0600

    Codigo

--- src/Models/Queue.js
+++ src/Models/Queue.js
@@ -1,23 +1,53 @@
+import{Node} from'./Node.js'
 export class Queue {
-    constructor() {
-      this.items = [];
-    }
-  
-    enqueue(element) {
-      this.items.push(element);
-    }
-  
-    dequeue() {
-      if (this.isEmpty()) return "Underflow";
-      return this.items.shift();
-    }
-  
-    isEmpty() {
-      return this.items.length === 0;
-    }
-  
-    printQueue() {
-      return this.items.join('\n');
-    }
+  constructor() {
+    this.front = null;
+    this.rear = null;
+    this.size = 0;
   }
-  
+
+  enqueue(element) {
+    let newNode = new Node(element);
+
+    if (this.isEmpty()) {
+      this.front = newNode;
+    } else {
+      this.rear.next = newNode;
+    }
+
+    this.rear = newNode;
+    this.size++;
+  }
+
+  dequeue() {
+    if (this.isEmpty()) {
+      return "Underflow";
+    }
+
+    let removedElement = this.front.element;
+    this.front = this.front.next;
+    this.size--;
+
+    if (this.isEmpty()) {
+      this.rear = null;
+    }
+
+    return removedElement;
+  }
+
+  isEmpty() {
+    return this.size === 0;
+  }
+
+  printQueue() {
+    let current = this.front;
+    let result = '';
+
+    while (current) {
+      result += current.element + '\n';
+      current = current.next;
+    }
+
+    return result;
+  }
+}
